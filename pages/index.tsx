4d7c394e202e38eb6068dd3d374e22f213831af9
zhenfeng-zhu <codeless98@163.com>
2023-12-31 09:53:00 +0800

    fix lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@
 // import LinkMaker from "../islands/LinkMaker.tsx";
 import Header from '@/components/Header';
 import LinkMaker from '@/components/LinkMaker';
-import { Html, Head, Main, NextScript } from 'next/document'
 
 
 export default function Home() {
